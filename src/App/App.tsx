ea1d91a7dc0f62668357440e40c1c66060bf58bf
Igor Maxsimov <maxsimovwork@gmail.com>
2023-01-16 21:25:02 +0700

    Добавил стилизацию.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { connect } from "react-redux";
 import { TodoAdd, ToggleTodo, setVisibilityFilter } from "../action/index";
 import { Todo, Counter } from "../types/todoTypes"
+import "../styles/App.css";
 
 interface AppPropType {
 	todos: Todo[];
@@ -16,12 +17,25 @@ interface AppPropType {
 const App: React.FC<AppPropType> = ({ todos, counter, currentFilter, onAddTodo, onToggle, onFilter }) => {
 
   const [inputValue, setInputValue] = useState("");
+  const [validationMessage, setValidationMessage] = useState("");
 
   const addTodo = () => {
-    if (inputValue.length <= 5) {
+    debugger;
+
+    if (inputValue.length === 0) {
+      setValidationMessage("Ошибка. Поле обязательно для заполнения"); 
+      return 
+    };
+
+    if (inputValue.length > 10)  { 
+      setValidationMessage("Ошибка. Длина должна быть меньше или равной 10 символам"); 
+      return 
+    };
+
+    setValidationMessage("");
+
       onAddTodo(inputValue);
       setInputValue("");
-    }
 	};
 
   const toggle = (id: number) => {
@@ -35,25 +49,34 @@ const App: React.FC<AppPropType> = ({ todos, counter, currentFilter, onAddTodo, 
   function handleInputChange(event: any) {
     setInputValue(event.target.value);
   };
-  let validationMessage = "";
-
-  if (inputValue.length > 5) {
-    validationMessage = "Ошибка. Длина должна быть меньше или равной 5 символам"
-  }
 
     return (
-      <div className="App">
-        <input type={"text"} value= { inputValue } onChange={ handleInputChange }></input>
-        { validationMessage }
-        <button onClick={() => addTodo()}>Добавить задачу</button>
-        |
-        <button style={{ color: "SHOW_ALL" === currentFilter ? "blue" : "black"}} onClick={() => filter("SHOW_ALL")}>Все</button>
-        <button style={{ color: "SHOW_COMPLETED" === currentFilter ? "blue" : "black"}} onClick={() => filter("SHOW_COMPLETED")}>Выполенные { counter.completedCount }</button>
-        <button style={{ color: "SHOW_INCOMPLETED" === currentFilter ? "blue" : "black"}} onClick={() => filter("SHOW_INCOMPLETED")}>Не выполненные { counter.incompletedCount }</button>
+      <div className="app">
+        <div className="app__input">
+          <div className="app__input-field">
+            <input className="text-field__input" type={"text"} value= { inputValue } onChange={ handleInputChange }></input>
+            <button className="custom-btn btn-4" onClick={() => addTodo()}>Добавить задачу</button>
+          </div>
+          <div className="break"></div>
+          <div >
+            { <span className="danger">{validationMessage}</span>  }
+          </div>
+        </div>
+        <div className="app__filter">
+          <button className={ currentFilter === "SHOW_ALL" ? "glow-button" : "deactive-button" } onClick={() => filter("SHOW_ALL")}>Все</button>
+          <button className={ currentFilter === "SHOW_COMPLETED" ? "glow-button" : "deactive-button" } onClick={() => filter("SHOW_COMPLETED")}>Выполенные { counter.completedCount }</button>
+          <button className={ currentFilter === "SHOW_INCOMPLETED" ? "glow-button" : "deactive-button" } onClick={() => filter("SHOW_INCOMPLETED")}>Не выполненные { counter.incompletedCount }</button>
+        </div>
         <div>
-          {
-            todos.map((elem: Todo) => { return <p> <span onClick={() => toggle(elem.id)}>{elem.text} {elem.completed ? "Выполено" : "Не выполнено"}</span></p> })
-          }
+          <ol className="rounded">
+            {
+              todos.map((elem: Todo) => { 
+                return <li key={elem.id}> 
+                <a className={  elem.completed ? "rounded-positive" : "rounded-negative" } onClick={() => toggle(elem.id)}>{elem.text}</a>
+              </li> }
+              )
+            }
+          </ol>
         </div>
       </div>
     );
